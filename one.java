9cfe7c90820b4c3b3dfb037fa25692746bf0a6e2
devang6911 <129258488+devang6911@users.noreply.github.com>
2023-04-19 12:11:14 +0530

    Update one.java

--- one.java
+++ one.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World spt tk,572104"); 
+        System.out.println("Hello, World spt tk,57210"); 
     }
 }
